8e633a1df4151591966c8ebea3968d5bd8000021
Alessandro Stein <alessandro@alessandrostein.com>
2014-05-12 11:38:06 -0300

    Laboratorio 1

---
+++ AppAND1GlobalcodeAlessandro/gen/com/example/appand1globalcodealessandro/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.appand1globalcodealessandro;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
